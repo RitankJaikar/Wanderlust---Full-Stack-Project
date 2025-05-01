c23f4f209a43a5031b4c8f9932c78ff6dbc2374a
RitankJaikar <ritank.jaikar@gmail.com>
2025-05-01 12:57:52 +0530

    New Commit

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ const listingRouter = require("./routes/listing");
 const reviewRouter = require("./routes/review");
 const expressSession = require("express-session");
 const flash = require("connect-flash");
-// const passport = require("passport");
-// const LocalStrategy = require("passport-local");
-// const User = require("./models/user");
-// const userRouter = require("./routes/user");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
+const userRouter = require("./routes/user");
 const connectMongo = require("connect-mongo");
 
 // Connect to DB
